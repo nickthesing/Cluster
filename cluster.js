26ec20dc53ce202593cd5170b0bbc4242e803763
nickthesing <nickthesing@gmail.com>
2014-12-29 15:34:57 +0100

    Update

--- cluster.js
+++ cluster.js
@@ -1,3 +1,32 @@
 if ( Meteor.isClient ) {
-    
+
+
+    // Body events
+    Template.body.events({
+        "submit .js-create-chat": function(event) {
+            var name = event.target.text.value;
+
+            // check for empty input eg. no name
+            if ( name === '' ) return;
+
+            // call method
+            Meteor.call('addChat', name);
+
+            // remove value from input
+            event.target.text.value = '';
+            return false;
+        }
+    });
 }
+
+// Meteor Methods
+Meteor.methods({
+    "addChat": function(name) {
+        // Check if user is logged in
+        // if ( Meteor.userId() ) {
+        //     throw new Meteor.error("not-authorized");
+        // }
+
+        console.log(name);
+    }
+})
